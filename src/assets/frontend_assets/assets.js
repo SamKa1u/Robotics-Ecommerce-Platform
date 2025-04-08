0c84e133b934ed32293c4f7f4b34afbf86a92d8c
Samuel Kalu <samkalu@ttu.edu>
2025-04-07 19:07:44 -0500

    upload files

---
+++ src/assets/frontend_assets/assets.js
@@ -0,0 +1,262 @@
+import p_img1 from './p_img1.png'
+import p_img1_1 from './p_img1_1.webp'
+import p_img1_2 from './p_img1_2.webp'
+import p_img2 from './p_img2.png'
+import p_img3 from './p_img3.png'
+import p_img4_1 from './p_img4_1.png'
+import p_img4_2 from './p_img4_2.png'
+import p_img4_3 from './p_img4_3.png'
+import p_img4_4 from './p_img4_4.png'
+import p_img5 from './p_img5.png'
+import p_img6 from './p_img6.png'
+import p_img7 from './p_img7.png'
+import p_img8 from './p_img8.png'
+import p_img9 from './p_img9.png'
+import p_img10 from './p_img10.png'
+import p_img11 from './p_img11.png'
+import p_img12 from './p_img12.png'
+import p_img13 from './p_img13.png'
+import p_img14 from './p_img14.png'
+import p_img15 from './p_img15.png'
+import p_img16 from './p_img16.png'
+import p_img17 from './p_img17.png'
+import p_img18 from './p_img18.png'
+import p_img19 from './p_img19.png'
+import p_img20 from './p_img20.png'
+import p_img21 from './p_img21.png'
+import p_img22 from './p_img22.png'
+import p_img23 from './p_img23.png'
+import p_img24 from './p_img24.png'
+import p_img25 from './p_img25.png'
+import p_img26 from './p_img26.png'
+import p_img27 from './p_img27.png'
+import p_img28 from './p_img28.png'
+import p_img29 from './p_img29.png'
+import p_img30 from './p_img30.png'
+import p_img31 from './p_img31.png'
+import p_img32 from './p_img32.png'
+import p_img33 from './p_img33.png'
+import p_img34 from './p_img34.png'
+import p_img35 from './p_img35.png'
+import p_img36 from './p_img36.png'
+import p_img37 from './p_img37.png'
+import p_img38 from './p_img38.png'
+import p_img39 from './p_img39.png'
+import p_img40 from './p_img40.png'
+import p_img41 from './p_img41.png'
+import p_img42 from './p_img42.png'
+import p_img43 from './p_img43.png'
+import p_img44 from './p_img44.png'
+import p_img45 from './p_img45.png'
+import p_img46 from './p_img46.png'
+import p_img47 from './p_img47.png'
+import p_img48 from './p_img48.png'
+import p_img49 from './p_img49.png'
+import p_img50 from './p_img50.png'
+import p_img51 from './p_img51.png'
+import p_img52 from './p_img52.png'
+
+
+import logo from './logo.png'
+import hero_img from './hero_img.png'
+import cart_icon from './cart_icon.png'
+import bin_icon from './bin_icon.png'
+import dropdown_icon from './dropdown_icon.png'
+import exchange_icon from './exchange_icon.png'
+import profile_icon from './profile_icon.png'
+import quality_icon from './quality_icon.png'
+import search_icon from './search_icon.png'
+import star_dull_icon from './star_dull_icon.png'
+import star_icon from './star_icon.png'
+import support_img from './support_img.png'
+import menu_icon from './menu_icon.png'
+import about_img from './about_img.png'
+import contact_img from './contact_img.png'
+import razorpay_logo from './razorpay_logo.png'
+import stripe_logo from './stripe_logo.png'
+import cross_icon from './cross_icon.png'
+
+export const assets = {
+    logo,
+    hero_img,
+    cart_icon,
+    dropdown_icon,
+    exchange_icon,
+    profile_icon,
+    quality_icon,
+    search_icon,
+    star_dull_icon,
+    star_icon,
+    bin_icon,
+    support_img,
+    menu_icon,
+    about_img,
+    contact_img,
+    razorpay_logo,
+    stripe_logo,
+    cross_icon
+}
+
+export const products = [
+    {
+        _id: "aaaaa",
+        name: "MechDog Hiwonder Open Source AI Robot",
+        description: "A Robot Dog with ESP32 Controller, High-Speed Coreless Servos Support Scratch, Arduino, and Python - Standard Kit.",
+        price: 349.99,
+        image: [p_img1, p_img1_1, p_img1_2],
+        category: "Legged Robots",
+        subCategory: "Quadruped",
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaab",
+        name: "Lynxmotion Biped Robot",
+        description: "Robot chassis only; no servos or electronics.\n" +
+            "Biped legs feature 6 degrees of freedom.\n" +
+            "Meant to be used with BotBoarduino + SSC-32/32U\n" +
+            "The Lynxmotion Biped Robot Scout (No Servos) is a twelve servo biped walker featuring six degrees of freedom (DOF) per leg. The advanced leg design features incredible flexibility. The chassis and servos are sold separately for those who want to use their own electronics. Note that there is currently no sample code available.",
+        price: 1.,
+        image: [p_img2],
+        category: "Legged Robots",
+        subCategory: "Bipedal",
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aaaac",
+        name: "",
+        description: "",
+        price: 1.,
+        image: [p_img3],
+        category: "Wheeled Robots",
+        subCategory: "Tracks",
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aaaad",
+        name: "",
+        description: "",
+        price: 1.,
+        image: [p_img4_1,p_img4_2,p_img4_3,p_img4_4],
+        category: "Wheeled Robots",
+        subCategory: "4-WD",
+        date: 1716634345448,
+        bestseller: true
+    },
+{
+        _id: "aaaae",
+        name: "",
+        description: "",
+        price: 1.,
+        image: [p_img5],
+        category: "Wheeled Robots",
+        subCategory: "2-WD",
+        date: 1716634345448,
+        bestseller: true
+    }
+    ,
+    {
+        _id: "aaaaf",
+        name: "",
+        description: "",
+        price: 1.,
+        image: [p_img6],
+        category: "Legged Robots",
+        subCategory: "Bipedal",
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aaaag",
+        name: "",
+        description: "",
+        price: 1.,
+        image: [p_img7],
+        category: "Wheeled Robots",
+        subCategory: "Tracks",
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaah",
+        name: "",
+        description: "",
+        price: 1.,
+        image: [p_img8],
+        category: "Legged Robots",
+        subCategory: "Bipedal",
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aaaai",
+        name: "",
+        description: "",
+        price: 1.,
+        image: [p_img9],
+        category: "Wheeled Robots",
+        subCategory: "Tracks",
+        date: 1716634345448,
+        bestseller: true
+    }
+    ,
+    {
+        _id: "aaaaj",
+        name: "",
+        description: "",
+        price: 1.,
+        image: [p_img10],
+        category: "Legged Robots",
+        subCategory: "Bipedal",
+        date: 1716634345448,
+        bestseller: true
+    }
+    ,
+    {
+        _id: "aaaak",
+        name: "",
+        description: "",
+        price: 1.,
+        image: [p_img11],
+        category: "Wheeled Robots",
+        subCategory: "Tracks",
+        date: 1716634345448,
+        bestseller: false
+    }
+    ,
+    {
+        _id: "aaaal",
+        name: "",
+        description: "",
+        price: 1.,
+        image: [p_img12],
+        category: "Legged Robots",
+        subCategory: "Bipedal",
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aaaam",
+        name: "",
+        description: "",
+        price: 1.,
+        image: [p_img13],
+        category: "Wheeled Robots",
+        subCategory: "Tracks",
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aaaan",
+        name: "",
+        description: "",
+        price: 1.,
+        image: [p_img14],
+        category: "Legged Robots",
+        subCategory: "Bipedal",
+        date: 1716634345448,
+        bestseller: false
+    }
+]
